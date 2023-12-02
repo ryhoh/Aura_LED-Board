b729a009ccd956ecfcb7ce226fc04ddfedaf9028
ryhoh <axis38akasira@gmail.com>
2023-12-02 09:28:21 +0900

    バージョン番号実装
    refs #13

--- src/App/variant/Profile.h
+++ src/App/variant/Profile.h
@@ -11,7 +11,14 @@
 // インクルード
 #include "App/Common.h"
 
-// マクロ定義
+/* ---------------- マクロ定義 ---------------- */
+/*                    ++------------- Product Name: "Luminous Board"     */
+/*                    || +----------- Major Version Decimal              */
+/*                    || | +--------- Minor Version Decimal              */
+/*                    || | | +------- Patch Version Decimal              */
+/*                    || | | |+------ Series Code: "Advanced"            */
+#define m_SW_VERSION "LBv2.2.0A"  /* Version ID                         */
+
 #define m_PROFILE_MAX_DESIGNED_PANEL_NUM (12)  // 設計上の限界パネル数
 
 #endif  /* _APP_PROFILE_H_ */
